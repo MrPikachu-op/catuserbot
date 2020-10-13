e7a9c7d3615aec4435161a3630b0cb17486a3b03
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-13 14:21:58 +0000

    pylint: auto fixes

--- userbot/helpers/__init__.py
+++ userbot/helpers/__init__.py
@@ -1,8 +1,8 @@
 from . import fonts
+from . import memeshelper as memes
 from .exceptions import CancelProcess
 from .functions import *
 from .memeifyhelpers import *
 from .progress import *
 from .qhelper import process
 from .utils import *
-from . import memeshelper as memes
