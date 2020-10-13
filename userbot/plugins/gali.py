ad31064da9915fe718353e30b5f870d90090e3ed
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-13 14:38:29 +0000

    pylint: auto fixes

--- userbot/plugins/gali.py
+++ userbot/plugins/gali.py
@@ -1,52 +1,53 @@
 import asyncio
-from .utils import admin_cmd, sudo_cmd , edit_or_reply
-from . import catmemes, CMD_HELP
+
+from . import CMD_HELP, catmemes
+from .utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="abuse$"))
-@bot.on(sudo_cmd(pattern="abuse$",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="abuse$", allow_sudo=True))
 async def abusing(abused):
     index = random.randint(0, len(catmemes.ABUSE_STRINGS) - 1)
     reply_text = catmemes.ABUSE_STRINGS[index]
-    await edit_or_reply(abused,reply_text)
+    await edit_or_reply(abused, reply_text)
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="abusehard$"))
-@bot.on(sudo_cmd(pattern="abusehard$",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="abusehard$", allow_sudo=True))
 async def fuckedd(abusehard):
     index = random.randint(0, len(catmemes.ABUSEHARD_STRING) - 1)
     reply_text = catmemes.ABUSEHARD_STRING[index]
-    await edit_or_reply(abusehard,reply_text)
+    await edit_or_reply(abusehard, reply_text)
 
 
-@bot.on(admin_cmd(outgoing=True,pattern="rape$"))
-@bot.on(sudo_cmd(pattern="rape$",allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="rape$"))
+@bot.on(sudo_cmd(pattern="rape$", allow_sudo=True))
 async def raping(raped):
     index = random.randint(0, len(catmemes.RAPE_STRINGS) - 1)
     reply_text = catmemes.RAPE_STRINGS[index]
-    await edit_or_reply(raped , reply_text)
+    await edit_or_reply(raped, reply_text)
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="fuck$"))
-@bot.on(sudo_cmd(pattern="fuck$",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="fuck$", allow_sudo=True))
 async def chutiya(fuks):
     index = random.randint(0, len(catmemes.CHU_STRINGS) - 1)
     reply_text = catmemes.FUK_STRINGS[index]
-    await edit_or_reply(fuks , reply_text)
+    await edit_or_reply(fuks, reply_text)
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="thanos$"))
-@bot.on(sudo_cmd(pattern="thanos$",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="thanos$", allow_sudo=True))
 async def thanos(thanos):
     index = random.randint(0, len(catmemes.THANOS_STRINGS) - 1)
     reply_text = catmemes.THANOS_STRINGS[index]
-    await edit_or_reply(thanos,reply_text)   
+    await edit_or_reply(thanos, reply_text)
 
 
-@bot.on(admin_cmd(outgoing=True,pattern="kiss$"))
-@bot.on(sudo_cmd(pattern="kiss$",allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="kiss$"))
+@bot.on(sudo_cmd(pattern="kiss$", allow_sudo=True))
 async def _(event):
-    catevent = await edit_or_reply(event , "`kiss`")
+    catevent = await edit_or_reply(event, "`kiss`")
     animation_interval = 0.2
     animation_ttl = range(100)
     animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵💋👰"]
@@ -55,10 +56,10 @@ async def _(event):
         await catevent.edit(animation_chars[i % 4])
 
 
-@bot.on(admin_cmd(outgoing=True,pattern="fuk$"))
-@bot.on(sudo_cmd(pattern="fuk$",allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="fuk$"))
+@bot.on(sudo_cmd(pattern="fuk$", allow_sudo=True))
 async def _(event):
-    catevent = await edit_or_reply(event , "`fuking....`")
+    catevent = await edit_or_reply(event, "`fuking....`")
     animation_interval = 0.2
     animation_ttl = range(100)
     animation_chars = ["👉       ✊️", "👉     ✊️", "👉  ✊️", "👉✊️💦"]
@@ -67,10 +68,10 @@ async def _(event):
         await catevent.edit(animation_chars[i % 4])
 
 
-@bot.on(admin_cmd(outgoing=True,pattern="sex$"))
-@bot.on(sudo_cmd(pattern="sex$",allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="sex$"))
+@bot.on(sudo_cmd(pattern="sex$", allow_sudo=True))
 async def _(event):
-    catevent = await edit_or_reply(event , "`sex`")
+    catevent = await edit_or_reply(event, "`sex`")
     animation_interval = 0.2
     animation_ttl = range(100)
     animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵👼👰"]
@@ -79,7 +80,8 @@ async def _(event):
         await catevent.edit(animation_chars[i % 4])
 
 
-CMD_HELP.update({
+CMD_HELP.update(
+    {
         "gali": "**plugin : **`gali`\
         \n\n**Commands :**\
         \n  •  `abuse`\
@@ -94,4 +96,5 @@ CMD_HELP.update({
         \n__First 5 are random gali string generaters__\
         \n__Last 3 are animations\
         "
-})
+    }
+)
