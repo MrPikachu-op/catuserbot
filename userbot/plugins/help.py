3f1fc28229a6578a3f4ffa78ea277f837a404cc1
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-14 02:22:04 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -16,17 +16,19 @@ async def cmd_list(event):
         reply_to_id = event.reply_to_msg_id
     input_str = event.pattern_match.group(1)
     if input_str == "text":
-        string = "Total {count} commands found in catuserbot\n\n"
+        string = "Total {count} commands found in {plugincount} of catuserbot\n\n"
         catcount = 0
+        plugincount = 0
         for i in sorted(CMD_LIST):
-            string += "Command found in Plugin " + i + " are \n"
+            plugincount += 1
+            string += f"{plugincount}) Command found in Plugin " + i + " are \n"
             for iter_list in CMD_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
                 catcount += 1
             string += "\n"
         if len(string) > 4095:
-            data = string.format(count=catcount)
+            data = string.format(count=catcount,plugincount = plugincount)
             key = (
                 requests.post(
                     "https://nekobin.com/api/documents", json={"content": data}
@@ -39,7 +41,7 @@ async def cmd_list(event):
             reply_text = f"All commands of the catuserbot are [here]({url})"
             await event.edit(reply_text)
             return
-        await event.edit(string.format(count=catcount))
+        await event.edit(string.format(count=catcount,plugincount = plugincount))
         return
     if input_str:
         if input_str in CMD_LIST:
