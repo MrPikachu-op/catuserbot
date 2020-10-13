45708757812f48993be30dc984ee52687ffd73e8
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-13 20:37:31 +0000

    pylint: auto fixes

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -47,7 +47,7 @@ async def cmd_list(event):
                 string += f"  •  `{i}`"
                 string += "\n"
                 catcount += 1
-            await event.edit(string.format(count=catcount,input_str=input_str))
+            await event.edit(string.format(count=catcount, input_str=input_str))
         else:
             await event.edit(input_str + " is not a valid plugin!")
     else:
