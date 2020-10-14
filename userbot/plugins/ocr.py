f1dc8c1a42b31f583fc7bc8cfb5583e5a47e7c95
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-14 14:13:16 +0530

    Update ocr.py

--- userbot/plugins/ocr.py
+++ userbot/plugins/ocr.py
@@ -67,7 +67,7 @@ async def ocr(event):
 CMD_HELP.update(
     {
         "ocr": "**Plugin : **`ocr`\
-        \n\n**Plugin : **`.ocr <language>`\
+        \n\n**Syntax : **`.ocr <language>`\
         \n**Function : **Reply to an image or sticker to extract text from it.\n\nGet language codes from [here](https://ocr.space/ocrapi)"
     }
 )
