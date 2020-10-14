de63ce5542b4f5f66807774851bd07a76e828d72
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-14 18:41:01 +0530

    Update linkpreview.py

--- userbot/plugins/linkpreview.py
+++ userbot/plugins/linkpreview.py
@@ -17,7 +17,7 @@ async def _(event):
         await edit_or_reply(event , "```Reply to a Link```")
         return
     chat = "@chotamreaderbot"
-    acatevent = wait edit_or_reply(event ,"```Processing```")
+    catevent = await edit_or_reply(event ,"```Processing```")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
