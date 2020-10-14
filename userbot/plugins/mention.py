22b402cde1cd322fab8740edfaa45a594ba41893
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-14 21:24:26 +0000

    pylint: auto fixes

--- userbot/plugins/mention.py
+++ userbot/plugins/mention.py
@@ -1,5 +1,4 @@
-
-#Some are ported from uniborg By: @INF1N17Y
+# Some are ported from uniborg By: @INF1N17Y
 
 
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -8,7 +7,7 @@ from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @bot.on(admin_cmd(pattern="tagall$"))
-@bot.on(sudo_cmd(pattern="tagall$",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="tagall$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -24,7 +23,7 @@ async def _(event):
 
 
 @bot.on(admin_cmd(pattern="all (.*)"))
-@bot.on(sudo_cmd(pattern="all (.*)",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="all (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -34,7 +33,7 @@ async def _(event):
     input_str = event.pattern_match.group(1)
 
     if not input_str:
-        return await edit_or_reply(event , "what should i do try `.all hello`.")
+        return await edit_or_reply(event, "what should i do try `.all hello`.")
 
     mentions = input_str
     chat = await event.get_input_chat()
@@ -45,13 +44,15 @@ async def _(event):
 
 
 @bot.on(admin_cmd(pattern="admins$"))
-@bot.on(sudo_cmd(pattern="admins$",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="admins$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     mentions = "@admin: **Spam Spotted**"
     chat = await event.get_input_chat()
-    async for x in event.client.iter_participants(chat, filter=ChannelParticipantsAdmins):
+    async for x in event.client.iter_participants(
+        chat, filter=ChannelParticipantsAdmins
+    ):
         mentions += f"[\u2063](tg://user?id={x.id})"
     reply_message = None
     if event.reply_to_msg_id:
@@ -63,7 +64,7 @@ async def _(event):
 
 
 @bot.on(admin_cmd(pattern="men (.*)"))
-@bot.on(sudo_cmd(pattern="men (.*)",allow_sudo=True))
+@bot.on(sudo_cmd(pattern="men (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -76,4 +77,4 @@ async def _(event):
         await event.delete()
         await event.client.send_message(event.chat_id, caption, parse_mode="HTML")
     else:
-        await edit_or_reply(event , "Reply to user with `.mention <your text>`")
+        await edit_or_reply(event, "Reply to user with `.mention <your text>`")
