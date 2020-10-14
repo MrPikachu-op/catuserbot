aed375f699e2d32ef2d14203c656f37c75234966
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-14 13:11:51 +0000

    pylint: auto fixes

--- userbot/plugins/linkpreview.py
+++ userbot/plugins/linkpreview.py
@@ -1,23 +1,23 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from ..utils import admin_cmd , edit_or_reply , sudo_cmd
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
-@bot.on(admin_cmd(pattern="ctg$",outgoing=True))
-@bot.on(sudo_cmd(pattern="ctg$",allow_sudo=True))
+@bot.on(admin_cmd(pattern="ctg$", outgoing=True))
+@bot.on(sudo_cmd(pattern="ctg$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     reply_message = await event.get_reply_message()
     if not reply_message:
-        await edit_or_reply(event ,"```Reply to a Link.```")
+        await edit_or_reply(event, "```Reply to a Link.```")
         return
     if not reply_message.text:
-        await edit_or_reply(event , "```Reply to a Link```")
+        await edit_or_reply(event, "```Reply to a Link```")
         return
     chat = "@chotamreaderbot"
-    catevent = await edit_or_reply(event ,"```Processing```")
+    catevent = await edit_or_reply(event, "```Processing```")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -27,7 +27,9 @@ async def _(event):
             response = await response
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await catevent.edit("`RIP Check Your Blacklist Boss and unblock @chotamreaderbot`")
+            await catevent.edit(
+                "`RIP Check Your Blacklist Boss and unblock @chotamreaderbot`"
+            )
             return
         if response.text.startswith(""):
             await catevent.edit("Am I Dumb Or Am I Dumb?")
