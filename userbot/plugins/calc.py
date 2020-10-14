672330b35ebaba341123a32f99d21ce84fd48f00
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-14 19:58:56 +0530

    Update calc.py

--- userbot/plugins/calc.py
+++ userbot/plugins/calc.py
@@ -4,10 +4,11 @@ import sys
 import traceback
 
 from .. import CMD_HELP
-from ..utils import admin_cmd, edit_or_reply
+from ..utils import admin_cmd, edit_or_reply,sudo_cmd
 
 
-@borg.on(admin_cmd(pattern="calc (.*)"))
+@bot.on(admin_cmd(pattern="calc (.*)"))
+@bot.on(sudo_cmd(pattern="calc (.*)",allow_sudo=True))
 async def _(car):
     cmd = car.text.split(" ", maxsplit=1)[1]
     event = await edit_or_reply(car, "Calculating ...")
@@ -50,7 +51,8 @@ async def aexec(code, event):
 
 CMD_HELP.update(
     {
-        "calc": "**SYNTAX : **`.calc` your equation :\
-      \n**USAGE : **solves the given maths equation by bodmass rule. "
+        "calc": "**Plugin : **`calc`\
+        \n\n**Syntax : **`.calc expression` \
+        \n**Function : **solves the given maths equation by bodmass rule. "
     }
 )
