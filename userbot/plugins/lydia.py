66eb87dd81472ea1a6bbb5bb5ef3a738d712aa7a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-15 02:36:47 +0530

    Update lydia.py

--- userbot/plugins/lydia.py
+++ userbot/plugins/lydia.py
@@ -106,7 +106,7 @@ async def on_new_message(event):
             # Try to think a thought.
             try:
                 async with event.client.action(event.chat_id, "location"):
-                    await asyncio.sleep(5)
+                    await asyncio.sleep(2)
                     output = lydia.think_thought(session_id, query)
                     await event.reply(output)
             except cf.exception.CoffeeHouseError as e:
