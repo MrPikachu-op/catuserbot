9c4a0d875911b9184ee9a24e0ed3d2f3d9db7b7f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-15 00:03:17 +0530

    Update github.py

--- userbot/plugins/github.py
+++ userbot/plugins/github.py
@@ -130,7 +130,7 @@ CMD_HELP.update(
         \n**Function : ** __Get information about an user on GitHub of given username__\
         \n\n**Syntax : **`.commit reply to python file to upload to github`\
         \n**Function : **__It uploads the given file to your github repo(to userbot/plugins folder)\
-        \nTo work commit plugin set `GITHUB_ACCESS_TOKEN` and `GIT_REPO_NAME` Variables in Heroku vars First__
+        \nTo work commit plugin set `GITHUB_ACCESS_TOKEN` and `GIT_REPO_NAME` Variables in Heroku vars First__\
     "
     }
 )
