c8e7b90e44308c0c38374bd9e9f699204a6d5804
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-10-17 22:18:01 +0530

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -8,9 +8,12 @@ from . import ALIVE_NAME, CMD_HELP, CMD_LIST, SUDO_LIST, yaml_format
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
+HELPTYPE = Config.HELP_INLINETYPE or False
+
 
 @bot.on(admin_cmd(outgoing=True, pattern="help ?(.*)"))
 async def cmd_list(event):
+    global HELPTYPE
     reply_to_id = None
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
@@ -61,7 +64,7 @@ async def cmd_list(event):
             await asyncio.sleep(3)
             await event.delete()
     else:
-        if Config.HELP_INLINETYPE is None:
+        if HELPTYPE:
             help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
                           \nUserbot Helper to reveal all the plugin names\
                           \n__Do__ `.help` __plugin_name for commands, in case popup doesn't appear.__\
@@ -174,14 +177,32 @@ async def info(event):
 @bot.on(sudo_cmd(pattern="dc$", allow_sudo=True))
 async def _(event):
     result = await bot(functions.help.GetNearestDcRequest())
-    result = (
-        yaml_format(result)
-        + "\n\n**List Of Telegram Data Centres:**\
-                                    \nDC1 : Miami FL, USA\
-                                    \nDC2 : Amsterdam, NL\
-                                    \nDC3 : Miami FL, USA\
-                                    \nDC4 : Amsterdam, NL\
-                                    \nDC5 : Singapore, SG\
-                                    "
+    result = (yaml_format(result)
+                + "\n\n**List Of Telegram Data Centres:**\
+                \nDC1 : Miami FL, USA\
+                \nDC2 : Amsterdam, NL\
+                \nDC3 : Miami FL, USA\
+                \nDC4 : Amsterdam, NL\
+                \nDC5 : Singapore, SG\
+                "
     )
     await edit_or_reply(event, result)
+
+@bot.on(admin_cmd(outgoing=True, pattern="setinline (True|False)"))
+async def _(event):
+    global HELPTYPE
+    type = event.pattern_match.group(1)
+    if HELPTYPE:
+        if type:
+            await event.edit("`inline mode is already enabled`")
+        else:
+            HELPTYPE = type 
+            await event.edit("`inline mode is disabled`") 
+    else:
+        if type:
+            HELPTYPE = type
+            await event.edit("`inline mode is enabled`")
+        else:
+            await event.edit("`inline mode is already disabled`") 
+
+
